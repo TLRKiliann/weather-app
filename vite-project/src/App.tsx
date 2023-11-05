baf9706a0850ccd61e67a96817a23e254a1d957e
TLRKiliann <philogenie@protonmail.com>
2023-11-05 13:43:15 +0100

    call api replaced in App.tsx & alert created

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,13 +1,65 @@
+import { useEffect, useState } from "react";
+import Weather from './components/Weather';
 import DateHour from './components/DateHour'
 import LineChart from "./components/LineChart"
-import DataWeather from './components/DataWeather'
 import AlertSection from './components/AlertSection'
 import SonarStorm from './components/SonarStorm'
 import EmailSender from './components/EmailSender'
 import meteo from '/images/meteo.jpg'
 import apiLogo from '/images/weatherapi_logo.jpg'
 
+type DataProps = {
+  location: {
+    name: string;
+    country: string;
+  }
+  current: {
+    temp_c: number;
+    feelslike_c: number;
+    wind_dir: number;
+    wind_kph: number;
+    wind_degree: number;
+    humidity: number;
+    precip_mm: number;
+    pressure_mb: number;
+    uv: number;
+    air_quality: {
+        co: number;
+        no2: number;
+        o3: number;
+        so2: number;
+    }
+    condition: {
+      text: string;
+      icon: string;
+    }
+  };
+}
+
 function App() {
+
+  const [data, setData] = useState<DataProps>();
+  const [loading, setLoading] = useState<boolean>(true)
+
+  console.log(data, "data")
+
+  const api_url_2 = "https://api.weatherapi.com/v1"
+  const api_key_2 = "f9cc795ef9de4a8182590531230411"
+
+  useEffect(() => {
+    if (loading === true) {
+      const fetchData = async() => {
+        await fetch(`${api_url_2}/current.json?key=${api_key_2}&q=Lausanne&aqi=yes`)
+        .then((res) => res.json())
+        .then((rslt) => {
+          setData(rslt)
+          console.log(rslt)
+          setLoading(false)
+        })
+      }
+      fetchData();
+    }
+  }, [])
 
   return (
     <>
@@ -27,11 +79,25 @@ function App() {
       
       <DateHour />
       <hr />
-      <DataWeather />
+
+      <section className='dataweather--section'>
+        {(typeof data?.location != 'undefined') ? (
+          <Weather weatherData={data} />
+        ) : (
+          <div>No response from API</div>
+        )}
+      </section>
+
       <hr />
       <LineChart />
       <hr />
-      <AlertSection />
+
+      {(typeof data?.location != 'undefined') ? (
+        <AlertSection weatherData={data} />
+      ) : (
+        <div>No response from API</div>
+      )}
+
       <hr />
       <SonarStorm />
       <hr />
--- vite-project/src/components/AlertSection.tsx
+++ vite-project/src/components/AlertSection.tsx
@@ -1,9 +1,76 @@
+type LittleDataProps = {
+  location: {
+    name: string;
+    country: string;
+  }
+  current: {
+    temp_c: number;
+    feelslike_c: number;
+    wind_dir: number;
+    wind_kph: number;
+    wind_degree: number;
+    humidity: number;
+    precip_mm: number;
+    pressure_mb: number;
+    uv: number;
+    air_quality: {
+        co: number;
+        no2: number;
+        o3: number;
+        so2: number;
+    }
+    condition: {
+      text: string;
+      icon: string;
+    }
+  };
+}
 
-export default function AlertSection() {
+type WeatherProps = {
+  weatherData: LittleDataProps;
+}
+
+export default function AlertSection({weatherData}: WeatherProps) {
+
+  const windData = weatherData.current.wind_kph;
+  const tempData = weatherData.current.temp_c;
+
+  if ((windData < 35) && (tempData < 27 && tempData > 2) ) {
+    return (
+      <section className="alert">
+        <h2 className="alert--title">No alert</h2>
+      </section>
+    )
+  }
+
   return (
-    <section className="mainalert">
+    <section className="alert">
 
-        <h2>Alert</h2>
+        <h2 className="alert--title">Alert</h2>
+
+        <div className="alert--container">
+
+          <div className="alert--box">
+
+            {windData >= 35 ? (
+              <span className="alert--span">Alert Wind ! {windData} km/h</span>
+            ) : (
+              null
+            )}
+
+          </div>
+
+          <div className="alert--box">
+
+            {tempData >= 27 || tempData <= 2 ? (
+              <span className="alert--span">Alert Temperature ! {tempData} °C</span>
+            ) : (
+              null
+            )}
+
+          </div>
+
+        </div>
 
     </section>
   )
