8171d58cc37d82ac32132fe11483e8f2e662b854
TLRKiliann <philogenie@protonmail.com>
2023-11-06 14:43:17 +0100

    project almost done

--- vite-project/src/components/AlertSection.tsx
+++ vite-project/src/components/AlertSection.tsx
@@ -1,33 +1,7 @@
-type LittleDataProps = {
-  location: {
-    name: string;
-    country: string;
-  }
-  current: {
-    temp_c: number;
-    feelslike_c: number;
-    wind_dir: number;
-    wind_kph: number;
-    wind_degree: number;
-    humidity: number;
-    precip_mm: number;
-    pressure_mb: number;
-    uv: number;
-    air_quality: {
-        co: number;
-        no2: number;
-        o3: number;
-        so2: number;
-    }
-    condition: {
-      text: string;
-      icon: string;
-    }
-  };
-}
+import { DataProps } from '../models/types'
 
 type WeatherProps = {
-  weatherData: LittleDataProps;
+  weatherData: DataProps;
 }
 
 export default function AlertSection({weatherData}: WeatherProps) {
@@ -35,7 +9,7 @@ export default function AlertSection({weatherData}: WeatherProps) {
   const windData = weatherData.current.wind_kph;
   const tempData = weatherData.current.temp_c;
 
-  if ((windData < 35) && (tempData < 27 && tempData > 2) ) {
+  if ((windData < 30) && (tempData < 27 && tempData > 2) ) {
     return (
       <section className="alert">
         <h2 className="alert--title">No alert</h2>
@@ -52,7 +26,7 @@ export default function AlertSection({weatherData}: WeatherProps) {
 
           <div className="alert--box">
 
-            {windData >= 35 ? (
+            {windData >= 30 ? (
               <span className="alert--span">Alert Wind ! {windData} km/h</span>
             ) : (
               null
--- vite-project/src/components/Weather.tsx
+++ vite-project/src/components/Weather.tsx
@@ -3,37 +3,10 @@ import { BiCurrentLocation } from "react-icons/bi";
 
 import { GiWindTurbine, GiWindsock } from "react-icons/gi"
 import { BsCloudRain } from "react-icons/bs"
-
-type LittleDataProps = {
-    location: {
-        name: string;
-        country: string;
-    }
-    current: {
-        temp_c: number;
-        feelslike_c: number;
-        wind_dir: number;
-        wind_kph: number;
-        wind_degree: number;
-        humidity: number;
-        precip_mm: number;
-        pressure_mb: number;
-        uv: number;
-        air_quality: {
-            co: number;
-            no2: number;
-            o3: number;
-            so2: number;
-        }
-        condition: {
-            text: string;
-            icon: string;
-        }
-    }
-}
+import { DataProps } from '../models/types'
 
 type WeatherProps = {
-    weatherData: LittleDataProps;
+    weatherData: DataProps;
 }
 
 export default function Weather({weatherData}: WeatherProps) {
